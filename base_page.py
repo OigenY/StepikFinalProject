88985b4b6fb73d39020e550a36b6475e2f359ad1
unknown <oigennespit@gmail.com>
2022-01-25 14:30:06 +0300

    Modified files according to the tasks

--- base_page.py
+++ base_page.py
@@ -1,8 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+        #инициализация работы экземпляра драйвера, юрлы, время ожидания
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
 
     def open(self):
         self.browser.get(self.url)
-        # ваша реализация
+        #экземпляр открывания юрлы
