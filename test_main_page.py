3a5d08debd6d80f09ce6793f6d69bac5ed459f9f
unknown <oigennespit@gmail.com>
2022-01-26 13:18:25 +0300

    Added some changes to files

--- test_main_page.py
+++ test_main_page.py
@@ -10,12 +10,16 @@
 import time
 
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+    link2 = "http://selenium1py.pythonanywhere.com/uk/accounts/login/"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    #page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
     page.should_be_login_link()
+    page2 = LoginPage(browser, link2)
+    page2.should_be_login_page()
     time.sleep(10)
